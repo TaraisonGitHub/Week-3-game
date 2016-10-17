c22dea9023f7c0dc791aacfc8bb1e7124b6ca5ea
Tara Baker <tarab@floridarealtors.org>
2016-10-17 16:06:37 -0400

    wrote basic logic for checking user input

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,56 @@
 
 var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var answerChoices = ["ostrich", "democracy", "below", "beautiful", "chasm", "sycamore", "toaster", "horizon", "falling"];
+var wordAnswer = answerChoices[Math.floor(Math.random() * answerChoices.length)];	console.log(wordAnswer);
+//___________________ GAME INITIALIZE __________________
 
-//___________________GAME INITIALIZE__________________
-
-var userPick = prompt("Pick a letter to fill in the word blanks.").toLowerCase();
-var wordAnswer = answerChoices[Math.floor(Math.random() * answerChoices.length)];
-//var index = alphabet.indexOf(userPick);
-
-//check to see if the user typed a letter
-if (isNaN(userPick)) {
-	console.log("they picked a letter");
-} else {
+document.onkeyup = function() {
+	var userPick = String.fromCharCode(event.keyCode).toLowerCase();
+	
+	if (isNaN(userPick)) {
+	console.log(userPick);
+	} else {
 	alert("Oops! It looks like you typed an invalid character. Remember, you must type a letter of the alphabet to play Hangman.");	
+ 	}
+
+//_________________ If the chosen word is ostrich _______________________
+
+	if (userPick === "o" || userPick === "s" || userPick === "t" || userPick === "r" || userPick === "i" || userPick === "c" || userPick === "h") {
+		if ((userPick === "o") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "s") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "t") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "r") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "i") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "c") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+		if ((userPick === "h") && (wordAnswer === "ostrich")) {
+		console.log("this letter is in the word ostrich");
+		}
+	} else {
+		console.log("wrong answer");
+	}
 }
 
-// This bit of code turns a user's keypress into a string
+
+//check to see if the user typed a letter
 /*
+
+
+// This bit of code turns a user's keypress into a string
+
 document.onkeyup = function(); {
 	var index = String.fromCharCode(event.keyCode).toLowerCase;
 }
 }
-*/
+*/u
